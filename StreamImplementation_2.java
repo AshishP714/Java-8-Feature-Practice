4e21f4286338ee46702f31f73be8c83920f18c32
Ashish Pardeshi <ashu.pardeshi714@gmail.com>
2026-04-10 13:46:36 +0530

    Add files via upload

---
+++ StreamImplementation_2.java
@@ -0,0 +1,28 @@
+package java8;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamImplementation_2 {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> l = new ArrayList<Integer>();
+		
+		l.add(0);
+		l.add(10);
+		l.add(20);
+		l.add(5);
+		l.add(15);
+		l.add(25);
+	
+		System.out.println("Before stream");
+		System.out.println(l);
+		
+		List<Integer> l1 = l.stream().map(i -> i * 2).collect(Collectors.toList());
+		
+		System.out.println("After stream");
+		System.out.println(l1);
+	}
+}
